76ac8cedd486eb80126fe86944de69b7e89f3240
Shoki Yokomizo <curin030204@yahoo.co.jp>
2021-01-05 23:51:43 +0900

    メッセージの詳細画面作成

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (!empty($_POST)) {
 $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDEr BY p.created DESC ');
 
 // 返信用リンク Re がクリックされた時に URLパラメータに res= と付くので、そこに値が入っていた場合
-if(isset($_REQUEST['res'])){
+if (isset($_REQUEST['res'])) {
   $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
   $response->execute(array($_REQUEST['res']));
 
@@ -83,9 +83,11 @@ if(isset($_REQUEST['res'])){
         <div class="msg">
           <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
           <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
-          <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
-            <a href="view.php?id=">
-              返信元のメッセージ</a>
+          <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+            <?php if (!$post['reply_message_id'] == 0) : ?>
+              <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'], ENT_QUOTES)); ?>">
+                返信元のメッセージ</a>
+            <?php endif; ?>
             [<a href="delete.php?id=" style="color: #F33;">削除</a>]
           </p>
         </div>
